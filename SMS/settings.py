688051d2c160be2499fcf37176e9d78a458b1335
alchinibas <alchinibas123@gmail.com>
2021-09-04 13:22:20 +0545

    Hides Secret Keys

--- SMS/settings.py
+++ SMS/settings.py
@@ -1,4 +1,4 @@
-
+from . import secretKeys
 """
 Django settings for SMS project.
 
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qzdo=&mt_v2m^vo)p(^6y-p(zb%g9^hbcy&v_u9!)q*t482=ef'
+SECRET_KEY = secretKeys.SMS_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
